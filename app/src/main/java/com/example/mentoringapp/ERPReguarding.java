04513c9f49fa016db847ad06cc85d6f6c17d2901
testgithubsudeep <130492844+testgithubsudeep@users.noreply.github.com>
2023-11-07 15:22:54 +0530

    sudeep
    added the functionality submit button to saved the erp reagrding successfully

--- app/src/main/java/com/example/mentoringapp/ERPReguarding.java
+++ app/src/main/java/com/example/mentoringapp/ERPReguarding.java
@@ -10,13 +10,24 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class ERPReguarding extends AppCompatActivity {
+    Button submiterp;
 
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erpreguarding);
+        submiterp=findViewById(R.id.submiterp);
+        submiterp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ERPReguarding.this, " ERP Submit Successfully", Toast.LENGTH_SHORT).show();
+
+                Intent intent=new Intent(ERPReguarding.this,ComplainStudent.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
