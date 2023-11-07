d437b254c030c4fdec6d72b53436f74e25295d18
testgithubsudeep <130492844+testgithubsudeep@users.noreply.github.com>
2023-11-07 15:34:58 +0530

    sudeep
    here  i just update the suject regarding complain in xml and java file

--- app/src/main/java/com/example/mentoringapp/SubjectReguarding.java
+++ app/src/main/java/com/example/mentoringapp/SubjectReguarding.java
@@ -2,13 +2,29 @@ package com.example.mentoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SubjectReguarding extends AppCompatActivity {
+    Button submiterp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_reguarding);
+        submiterp=findViewById(R.id.submiterp);
+        submiterp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SubjectReguarding.this, "Subject Submit Successfully", Toast.LENGTH_SHORT).show();
+
+                Intent intent=new Intent(SubjectReguarding.this,ComplainStudent.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
