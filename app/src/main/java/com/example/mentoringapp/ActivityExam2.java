ad398220dca683b723f727dca91d1402f37aa94e
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2023-11-13 12:29:25 +0530

    added the exam result database
    added the exam result database in activity and layout folder

---
+++ app/src/main/java/com/example/mentoringapp/ActivityExam2.java
@@ -0,0 +1,14 @@
+package com.example.mentoringapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityExam2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam2);
+    }
+}
